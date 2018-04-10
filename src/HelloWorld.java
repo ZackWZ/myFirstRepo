b2193cd8daec9cd9e33435385bedee24e7653f4d
ZackWZ <zackwong1993@gmail.com>
2018-04-09 21:23:08 -0700

    my first push

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println(infiniteCall(10));
+		System.out.println("MY FIRST PUSH");
 	}
 	
 	public static int infiniteCall(int a) {
